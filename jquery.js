c08bef22e79c1ccf3288a2a53d8de6de6ecc30ae
Sam Trapskin <sam.trapskin@totalexpert.com>
2019-03-21 19:26:41 -0500

    added main navbar-first dropdown functional

--- jquery.js
+++ jquery.js
@@ -2,4 +2,16 @@ $( document ).ready(readyNow)
 
 function readyNow() {
 console.log('Hi there!')
+
+
+
+    $('#first').on('click', () => {
+        $('.who-dropdown-content').toggle();
+        console.log('shown');
+    })
+
+    // $('#first').on('mouseleave', () => {
+    //     $('.who-dropdown-content').hide();
+    // })
+
 }
